9cd199ef659e3d898e4a3716b212969539be00d3
stone.wu <stone.wu@webeye.com>
2024-09-04 14:02:29 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+/*
+ * @Author: stone.wu stone.wu@webeye.com
+ * @Date: 2024-08-23 20:41:38
+ * @LastEditors: stone.wu stone.wu@webeye.com
+ * @LastEditTime: 2024-09-04 13:59:41
+ * @FilePath: /sample-site/app/layout.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Navbar } from "@/components/navbar";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body className={clsx("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen">
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">{children}</main>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@nextui-org/button";
+
+function openTab(url) {
+  let newTab = document.createElement("a");
+  newTab.href = url;
+  newTab.target = "_blank";
+  newTab.rel = "noopener noreferrer";
+  newTab.click();
+}
+
+export default function Home() {
+  function click() {
+    openTab("https://test.halocoupons.com");
+  }
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <Button onClick={click}>openTab</Button>
+    </section>
+  );
+}
